64f01240372ae657ac3869cc42c0f52f20739525
andoeviniko <andoevi.niko@gmail.com>
2023-10-04 20:52:56 +0400

    first commit

---
+++ day4/day4.py
@@ -0,0 +1,13 @@
+#1
+age = int(input ("enter your age: "))
+print ((age * 7/3)+ (4*9) )
+
+#2
+car_price = 7000
+print(car_price + (7000 * 62)/100)
+
+#anecdote
+waitress = (input("hello: "))
+waitress1 = (input("how can i help you?: "))
+waitress2 = (input("how many khinkali do you want?: "))
+print(waitress, waitress1, waitress2 + " pieces")
