45967a3f03ecf543f34d2c26d1e5d36d35584bb4
andoeviniko <andoevi.niko@gmail.com>
2023-10-08 15:12:01 +0400

    second commit

---
+++ day5/cw.py
@@ -0,0 +1,16 @@
+#cw_1
+name = input("what is your name? ")
+print("Hello, " + name)
+age = int(input("how old are you? "))
+
+
+for i in range(10):
+    print(name + " you will be " + str(age + i) + " years old in " + str(2024 + i))
+#cw1 end
+
+#cw2 
+seats = 10 
+while seats > 0: 
+  print(" ticket left" + str(seats)) 
+  seats = seats - 1 
+#cw2 end
---
+++ day5/hw.py
@@ -0,0 +1,17 @@
+#hw1
+father_age = int(input("enter your father's age: "))
+my_age = int(input("enter your age: "))
+
+for i in range(100):
+    print((father_age + i) / (my_age + i))
+#hw1 end
+
+
+#hw2
+i = 0
+for i in range(1,30):
+    if (i % 2 == 1):
+        print(str(i) + " Odd")
+    else:
+        print(str(i) + " even")    
+#hw2 end
